d34c9963bf72835de1b8597bbd872d406768c715
Klaus Gulhote Kuhlmann <129099809+klaus14k@users.noreply.github.com>
2024-03-06 18:16:29 -0300

    Delete dist directory

--- dist/src/schemas/session.schema.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sessionCreateSchema = void 0;
-const user_schema_1 = require("./user.schema");
-exports.sessionCreateSchema = user_schema_1.UserSchema.pick({ email: true, password: true });
